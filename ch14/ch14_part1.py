9a59a7bcb0068d64cf55a0d86b35be605e57dc45
rasbt <mail@sebastianraschka.com>
2021-12-22 20:51:31 -0600

    chapter 14

---
+++ ch14/ch14_part1.py
@@ -0,0 +1,503 @@
+# coding: utf-8
+
+
+import sys
+from python_environment_check import check_packages
+import torch
+import numpy as np
+import scipy.signal
+from torchvision.io import read_image
+import torch.nn as nn
+import torchvision 
+from torchvision import transforms 
+from torch.utils.data import Subset
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import os
+
+# # Machine Learning with PyTorch and Scikit-Learn  
+# # -- Code Examples
+
+# ## Package version checks
+
+# Add folder to path in order to load from the check_packages.py script:
+
+
+
+sys.path.insert(0, '..')
+
+
+# Check recommended package versions:
+
+
+
+
+
+d = {
+    'numpy': '1.21.2',
+    'scipy': '1.7.0',
+    'matplotlib': '3.4.3',
+    'torch': '1.8.0',
+    'torchvision': '0.9.0'
+}
+check_packages(d)
+
+
+# # Chapter 14: Classifying Images with Deep Convolutional Neural Networks (Part 1/2)
+
+# **Outline**
+# 
+# - [The building blocks of CNNs](#The-building-blocks-of-CNNs)
+#   - [Understanding CNNs and feature hierarchies](#Understanding-CNNs-and-feature-hierarchies)
+#   - [Performing discrete convolutions](#Performing-discrete-convolutions)
+#     - [Discrete convolutions in one dimension](#Discrete-convolutions-in-one-dimension)
+#     - [Padding inputs to control the size of the output feature maps](#Padding-inputs-to-control-the-size-of-the-output-feature-maps)
+#     - [Determining the size of the convolution output](#Determining-the-size-of-the-convolution-output)
+#     - [Performing a discrete convolution in 2D](#Performing-a-discrete-convolution-in-2D)
+#   - [Subsampling layers](#Subsampling-layers)
+# - [Putting everything together -- implementing a CNN](#Putting-everything-together----implementing-a-CNN)
+#   - [Working with multiple input or color channels](#Working-with-multiple-input-or-color-channels)
+#   - [Regularizing an NN with L2 regularization and dropout](#Regularizing-an-NN-with-L2-regularization-and-dropout)
+#   - [Loss functions for classification](#Loss-functions-for-classification)
+# - [Implementing a deep CNN using PyTorch](#Implementing-a-deep-CNN-using-PyTorch)
+#   - [The multilayer CNN architecture](#The-multilayer-CNN-architecture)
+#   - [Loading and preprocessing the data](#Loading-and-preprocessing-the-data)
+#   - [Implementing a CNN using the torch.nn module](#Implementing-a-CNN-using-the-torch.nn-module)
+#     - [Configuring CNN layers in PyTorch](#Configuring-CNN-layers-in-PyTorch)
+#     - [Constructing a CNN in PyTorch](#Constructing-a-CNN-in-PyTorch)
+
+
+
+
+
+# ##  The building blocks of convolutional neural networks
+# 
+# ### Understanding CNNs and feature hierarchies
+
+
+
+
+
+# 
+# 
+# ### Performing discrete convolutions
+# 
+# ### Discrete convolutions in one dimension
+# 
+# 
+
+
+
+
+
+
+
+
+
+# ### Padding inputs to control the size of the output feature maps
+# 
+# 
+
+
+
+
+
+# ### Determining the size of the convolution output
+
+
+
+
+print('PyTorch version:', torch.__version__)
+print('NumPy version: ', np.__version__)
+
+
+
+
+def conv1d(x, w, p=0, s=1):
+    w_rot = np.array(w[::-1])
+    x_padded = np.array(x)
+    if p > 0:
+        zero_pad = np.zeros(shape=p)
+        x_padded = np.concatenate(
+            [zero_pad, x_padded, zero_pad])
+    res = []
+    for i in range(0, int((len(x_padded) - len(w_rot))) + 1, s):
+        res.append(np.sum(
+            x_padded[i:i+w_rot.shape[0]] * w_rot))
+    return np.array(res)
+
+
+## Testing:
+x = [1, 3, 2, 4, 5, 6, 1, 3]
+w = [1, 0, 3, 1, 2]
+
+print('Conv1d Implementation:',
+      conv1d(x, w, p=2, s=1))
+
+print('Numpy Results:',
+      np.convolve(x, w, mode='same')) 
+
+
+# ### Performing a discrete convolution in 2D
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def conv2d(X, W, p=(0, 0), s=(1, 1)):
+    W_rot = np.array(W)[::-1,::-1]
+    X_orig = np.array(X)
+    n1 = X_orig.shape[0] + 2*p[0]
+    n2 = X_orig.shape[1] + 2*p[1]
+    X_padded = np.zeros(shape=(n1, n2))
+    X_padded[p[0]:p[0]+X_orig.shape[0],
+    p[1]:p[1]+X_orig.shape[1]] = X_orig
+
+    res = []
+    for i in range(0, int((X_padded.shape[0] - 
+                           W_rot.shape[0])/s[0])+1, s[0]):
+        res.append([])
+        for j in range(0, int((X_padded.shape[1] - 
+                               W_rot.shape[1])/s[1])+1, s[1]):
+            X_sub = X_padded[i:i+W_rot.shape[0],
+                             j:j+W_rot.shape[1]]
+            res[-1].append(np.sum(X_sub * W_rot))
+    return(np.array(res))
+
+X = [[1, 3, 2, 4], [5, 6, 1, 3], [1, 2, 0, 2], [3, 4, 3, 2]]
+W = [[1, 0, 3], [1, 2, 1], [0, 1, 1]]
+
+print('Conv2d Implementation:\n',
+    conv2d(X, W, p=(1, 1), s=(1, 1)))
+
+
+print('SciPy Results:\n',
+    scipy.signal.convolve2d(X, W, mode='same'))
+
+
+# ## Subsampling layers
+
+
+
+
+
+# ## Putting everything together – implementing a CNN
+# 
+# ### Working with multiple input or color channels
+# 
+# 
+
+
+
+
+
+# **TIP: Reading an image file**
+
+
+
+ 
+img = read_image('example-image.png') 
+ 
+print('Image shape:', img.shape)
+print('Number of channels:', img.shape[0])
+print('Image data type:', img.dtype)
+print(img[:, 100:102, 100:102])
+
+
+# ## Regularizing a neural network with L2 regularization and dropout
+# 
+# 
+
+
+
+
+
+
+
+loss_func = nn.BCELoss()
+loss = loss_func(torch.tensor([0.9]), torch.tensor([1.0]))
+l2_lambda = 0.001
+
+conv_layer = nn.Conv2d(in_channels=3, out_channels=5, kernel_size=5)
+l2_penalty = l2_lambda * sum([(p**2).sum() for p in conv_layer.parameters()])
+loss_with_penalty = loss + l2_penalty
+
+linear_layer = nn.Linear(10, 16)
+l2_penalty = l2_lambda * sum([(p**2).sum() for p in linear_layer.parameters()])
+loss_with_penalty = loss + l2_penalty
+
+
+# ## Loss Functions for Classification
+# 
+#  * **`nn.BCELoss()`**
+#    * `from_logits=False` 
+#    * `from_logits=True`
+# 
+#  * **`nn.CrossEntropyLoss()`**
+#    * `from_logits=False`
+#    * `from_logits=True`
+# 
+
+
+
+
+
+
+
+####### Binary Cross-entropy
+logits = torch.tensor([0.8])
+probas = torch.sigmoid(logits)
+target = torch.tensor([1.0])
+
+bce_loss_fn = nn.BCELoss()
+bce_logits_loss_fn = nn.BCEWithLogitsLoss()
+
+print(f'BCE (w Probas): {bce_loss_fn(probas, target):.4f}')
+print(f'BCE (w Logits): {bce_logits_loss_fn(logits, target):.4f}')
+
+ 
+####### Categorical Cross-entropy
+logits = torch.tensor([[1.5, 0.8, 2.1]])
+probas = torch.softmax(logits, dim=1)
+target = torch.tensor([2])
+
+cce_loss_fn = nn.NLLLoss()
+cce_logits_loss_fn = nn.CrossEntropyLoss()
+
+print(f'CCE (w Probas): {cce_logits_loss_fn(logits, target):.4f}')
+print(f'CCE (w Logits): {cce_loss_fn(torch.log(probas), target):.4f}')
+
+
+# ## Implementing a deep convolutional neural network using PyTorch
+# 
+# ### The multilayer CNN architecture
+
+
+
+
+
+# ### Loading and preprocessing the data
+
+
+
+image_path = './'
+transform = transforms.Compose([transforms.ToTensor()])
+
+mnist_dataset = torchvision.datasets.MNIST(root=image_path, 
+                                           train=True, 
+                                           transform=transform, 
+                                           download=False)
+
+mnist_valid_dataset = Subset(mnist_dataset, torch.arange(10000)) 
+mnist_train_dataset = Subset(mnist_dataset, torch.arange(10000, len(mnist_dataset)))
+mnist_test_dataset = torchvision.datasets.MNIST(root=image_path, 
+                                           train=False, 
+                                           transform=transform, 
+                                           download=False)
+
+
+
+
+batch_size = 64
+torch.manual_seed(1)
+train_dl = DataLoader(mnist_train_dataset, batch_size, shuffle=True)
+valid_dl = DataLoader(mnist_valid_dataset, batch_size, shuffle=False)
+
+
+# ### Implementing a CNN using the torch.nn module
+# 
+# #### Configuring CNN layers in PyTorch
+# 
+#  * **Conv2d:** `torch.nn.Conv2d`
+#    * `out_channels`
+#    * `kernel_size`
+#    * `stride`
+#    * `padding`
+#    
+#    
+#  * **MaxPool2d:** `torch.nn.MaxPool2d`
+#    * `kernel_size`
+#    * `stride`
+#    * `padding`
+#    
+#    
+#  * **Dropout** `torch.nn.Dropout`
+#    * `p`
+
+# ### Constructing a CNN in PyTorch
+
+
+
+model = nn.Sequential()
+model.add_module('conv1', nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, padding=2))
+model.add_module('relu1', nn.ReLU())        
+model.add_module('pool1', nn.MaxPool2d(kernel_size=2))   
+model.add_module('conv2', nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2))
+model.add_module('relu2', nn.ReLU())        
+model.add_module('pool2', nn.MaxPool2d(kernel_size=2))      
+
+x = torch.ones((4, 1, 28, 28))
+model(x).shape
+
+
+
+
+model.add_module('flatten', nn.Flatten()) 
+
+x = torch.ones((4, 1, 28, 28))
+model(x).shape
+
+
+
+
+model.add_module('fc1', nn.Linear(3136, 1024)) 
+model.add_module('relu3', nn.ReLU()) 
+model.add_module('dropout', nn.Dropout(p=0.5)) 
+
+model.add_module('fc2', nn.Linear(1024, 10)) 
+model.add_module('softmax', nn.Softmax(dim=1)) 
+ 
+
+
+
+
+device = torch.device("cuda:0")
+# device = torch.device("cpu")
+
+model = model.to(device) 
+
+
+
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+def train(model, num_epochs, train_dl, valid_dl):
+    loss_hist_train = [0] * num_epochs
+    accuracy_hist_train = [0] * num_epochs
+    loss_hist_valid = [0] * num_epochs
+    accuracy_hist_valid = [0] * num_epochs
+    for epoch in range(num_epochs):
+        model.train()
+        for x_batch, y_batch in train_dl:
+            x_batch = x_batch.to(device) 
+            y_batch = y_batch.to(device) 
+            pred = model(x_batch)
+            loss = loss_fn(pred, y_batch)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            loss_hist_train[epoch] += loss.item()*y_batch.size(0)
+            is_correct = (torch.argmax(pred, dim=1) == y_batch).float()
+            accuracy_hist_train[epoch] += is_correct.sum().cpu()
+
+        loss_hist_train[epoch] /= len(train_dl.dataset)
+        accuracy_hist_train[epoch] /= len(train_dl.dataset)
+        
+        model.eval()
+        with torch.no_grad():
+            for x_batch, y_batch in valid_dl:
+                x_batch = x_batch.to(device) 
+                y_batch = y_batch.to(device) 
+                pred = model(x_batch)
+                loss = loss_fn(pred, y_batch)
+                loss_hist_valid[epoch] += loss.item()*y_batch.size(0) 
+                is_correct = (torch.argmax(pred, dim=1) == y_batch).float() 
+                accuracy_hist_valid[epoch] += is_correct.sum().cpu()
+
+        loss_hist_valid[epoch] /= len(valid_dl.dataset)
+        accuracy_hist_valid[epoch] /= len(valid_dl.dataset)
+        
+        print(f'Epoch {epoch+1} accuracy: {accuracy_hist_train[epoch]:.4f} val_accuracy: {accuracy_hist_valid[epoch]:.4f}')
+    return loss_hist_train, loss_hist_valid, accuracy_hist_train, accuracy_hist_valid
+
+torch.manual_seed(1)
+num_epochs = 20
+hist = train(model, num_epochs, train_dl, valid_dl)
+
+
+
+
+x_arr = np.arange(len(hist[0])) + 1
+
+fig = plt.figure(figsize=(12, 4))
+ax = fig.add_subplot(1, 2, 1)
+ax.plot(x_arr, hist[0], '-o', label='Train loss')
+ax.plot(x_arr, hist[1], '--<', label='Validation loss')
+ax.set_xlabel('Epoch', size=15)
+ax.set_ylabel('Loss', size=15)
+ax.legend(fontsize=15)
+ax = fig.add_subplot(1, 2, 2)
+ax.plot(x_arr, hist[2], '-o', label='Train acc.')
+ax.plot(x_arr, hist[3], '--<', label='Validation acc.')
+ax.legend(fontsize=15)
+ax.set_xlabel('Epoch', size=15)
+ax.set_ylabel('Accuracy', size=15)
+
+plt.show()
+
+
+
+
+torch.cuda.synchronize()
+model_cpu = model.cpu()
+pred = model(mnist_test_dataset.data.unsqueeze(1) / 255.)
+is_correct = (torch.argmax(pred, dim=1) == mnist_test_dataset.targets).float()
+print(f'Test accuracy: {is_correct.mean():.4f}') 
+ 
+
+
+
+
+fig = plt.figure(figsize=(12, 4))
+for i in range(12):
+    ax = fig.add_subplot(2, 6, i+1)
+    ax.set_xticks([]); ax.set_yticks([])
+    img = mnist_test_dataset[i][0][0, :, :]
+    pred = model(img.unsqueeze(0).unsqueeze(1))
+    y_pred = torch.argmax(pred)
+    ax.imshow(img, cmap='gray_r')
+    ax.text(0.9, 0.1, y_pred.item(), 
+            size=15, color='blue',
+            horizontalalignment='center',
+            verticalalignment='center', 
+            transform=ax.transAxes)
+    
+plt.show()
+
+
+
+
+
+if not os.path.exists('models'):
+    os.mkdir('models')
+
+path = 'models/mnist-cnn.ph'
+torch.save(model, path)
+ 
+
+
+# ----
+# 
+# Readers may ignore the next cell.
+
+
+
+
+
+
+
+
+
---
+++ ch14/ch14_part2.py
@@ -0,0 +1,475 @@
+# coding: utf-8
+
+
+import sys
+from python_environment_check import check_packages
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision 
+from torchvision import transforms 
+from torch.utils.data import DataLoader
+from torch.utils.data import Subset
+
+# # Machine Learning with PyTorch and Scikit-Learn  
+# # -- Code Examples
+
+# ## Package version checks
+
+# Add folder to path in order to load from the check_packages.py script:
+
+
+
+sys.path.insert(0, '..')
+
+
+# Check recommended package versions:
+
+
+
+
+
+d = {
+    'numpy': '1.21.2',
+    'scipy': '1.7.0',
+    'matplotlib': '3.4.3',
+    'torch': '1.8.0',
+    'torchvision': '0.9.0'
+}
+check_packages(d)
+
+
+# # Chapter 14: Classifying Images with Deep Convolutional Neural Networks (Part 2/2)
+
+# **Outline**
+# 
+# - [Smile classification from face images using a CNN](#Constructing-a-CNN-in-PyTorch)
+#   - [Loading the CelebA dataset](#Loading-the-CelebA-dataset)
+#   - [Image transformation and data augmentation](#Image-transformation-and-data-augmentation)
+#   - [Training a CNN smile classifier](#Training-a-CNN-smile-classifier)
+# - [Summary](#Summary)
+
+# Note that the optional watermark extension is a small IPython notebook plugin that I developed to make the code reproducible. You can just skip the following line(s).
+
+
+
+
+
+
+
+# ## Gender classification from face images using CNN
+# 
+
+# ### Loading the CelebA dataset
+
+
+
+
+image_path = './'
+celeba_train_dataset = torchvision.datasets.CelebA(image_path, split='train', target_type='attr', download=False)
+celeba_valid_dataset = torchvision.datasets.CelebA(image_path, split='valid', target_type='attr', download=False)
+celeba_test_dataset = torchvision.datasets.CelebA(image_path, split='test', target_type='attr', download=False)
+
+print('Train set:', len(celeba_train_dataset))
+print('Validation set:', len(celeba_valid_dataset))
+print('Test set:', len(celeba_test_dataset))
+
+
+# ### Image transformation and data augmentation
+
+
+
+
+## take 5 examples
+
+fig = plt.figure(figsize=(16, 8.5))
+
+## Column 1: cropping to a bounding-box
+ax = fig.add_subplot(2, 5, 1)
+img, attr = celeba_train_dataset[0]
+ax.set_title('Crop to a \nbounding-box', size=15)
+ax.imshow(img)
+ax = fig.add_subplot(2, 5, 6)
+img_cropped = transforms.functional.crop(img, 50, 20, 128, 128)
+ax.imshow(img_cropped)
+
+## Column 2: flipping (horizontally)
+ax = fig.add_subplot(2, 5, 2)
+img, attr = celeba_train_dataset[1]
+ax.set_title('Flip (horizontal)', size=15)
+ax.imshow(img)
+ax = fig.add_subplot(2, 5, 7)
+img_flipped = transforms.functional.hflip(img)
+ax.imshow(img_flipped)
+
+## Column 3: adjust contrast
+ax = fig.add_subplot(2, 5, 3)
+img, attr = celeba_train_dataset[2]
+ax.set_title('Adjust constrast', size=15)
+ax.imshow(img)
+ax = fig.add_subplot(2, 5, 8)
+img_adj_contrast = transforms.functional.adjust_contrast(img, contrast_factor=2)
+ax.imshow(img_adj_contrast)
+
+## Column 4: adjust brightness
+ax = fig.add_subplot(2, 5, 4)
+img, attr = celeba_train_dataset[3]
+ax.set_title('Adjust brightness', size=15)
+ax.imshow(img)
+ax = fig.add_subplot(2, 5, 9)
+img_adj_brightness = transforms.functional.adjust_brightness(img, brightness_factor=1.3)
+ax.imshow(img_adj_brightness)
+
+## Column 5: cropping from image center 
+ax = fig.add_subplot(2, 5, 5)
+img, attr = celeba_train_dataset[4]
+ax.set_title('Center crop\nand resize', size=15)
+ax.imshow(img)
+ax = fig.add_subplot(2, 5, 10)
+img_center_crop = transforms.functional.center_crop(img, [0.7*218, 0.7*178])
+img_resized = transforms.functional.resize(img_center_crop, size=(218, 178))
+ax.imshow(img_resized)
+ 
+# plt.savefig('figures/14_14.png', dpi=300)
+plt.show()
+
+
+
+
+torch.manual_seed(1)
+
+fig = plt.figure(figsize=(14, 12))
+
+for i, (img, attr) in enumerate(celeba_train_dataset):
+    ax = fig.add_subplot(3, 4, i*4+1)
+    ax.imshow(img)
+    if i == 0:
+        ax.set_title('Orig.', size=15)
+        
+    ax = fig.add_subplot(3, 4, i*4+2)
+    img_transform = transforms.Compose([transforms.RandomCrop([178, 178])])
+    img_cropped = img_transform(img)
+    ax.imshow(img_cropped)
+    if i == 0:
+        ax.set_title('Step 1: Random crop', size=15)
+
+    ax = fig.add_subplot(3, 4, i*4+3)
+    img_transform = transforms.Compose([transforms.RandomHorizontalFlip()])
+    img_flip = img_transform(img_cropped)
+    ax.imshow(img_flip)
+    if i == 0:
+        ax.set_title('Step 2: Random flip', size=15)
+
+    ax = fig.add_subplot(3, 4, i*4+4)
+    img_resized = transforms.functional.resize(img_flip, size=(128, 128))
+    ax.imshow(img_resized)
+    if i == 0:
+        ax.set_title('Step 3: Resize', size=15)
+    
+    if i == 2:
+        break
+        
+# plt.savefig('figures/14_15.png', dpi=300)
+plt.show()
+
+
+
+
+get_smile = lambda attr: attr[18]
+ 
+transform_train = transforms.Compose([
+    transforms.RandomCrop([178, 178]),
+    transforms.RandomHorizontalFlip(),
+    transforms.Resize([64, 64]),
+    transforms.ToTensor(),
+])
+
+transform = transforms.Compose([
+    transforms.CenterCrop([178, 178]),
+    transforms.Resize([64, 64]),
+    transforms.ToTensor(),
+])
+
+
+
+
+
+celeba_train_dataset = torchvision.datasets.CelebA(image_path, 
+                                                   split='train', 
+                                                   target_type='attr', 
+                                                   download=False, 
+                                                   transform=transform_train,
+                                                   target_transform=get_smile)
+
+torch.manual_seed(1)
+data_loader = DataLoader(celeba_train_dataset, batch_size=2)
+
+fig = plt.figure(figsize=(15, 6))
+
+num_epochs = 5
+for j in range(num_epochs):
+    img_batch, label_batch = next(iter(data_loader))
+    img = img_batch[0]
+    ax = fig.add_subplot(2, 5, j + 1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_title(f'Epoch {j}:', size=15)
+    ax.imshow(img.permute(1, 2, 0))
+
+    img = img_batch[1]
+    ax = fig.add_subplot(2, 5, j + 6)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.imshow(img.permute(1, 2, 0))
+      
+    
+#plt.savefig('figures/14_16.png', dpi=300)
+plt.show()
+
+
+
+
+celeba_valid_dataset = torchvision.datasets.CelebA(image_path, 
+                                                   split='valid', 
+                                                   target_type='attr', 
+                                                   download=False, 
+                                                   transform=transform,
+                                                   target_transform=get_smile)
+
+celeba_test_dataset = torchvision.datasets.CelebA(image_path, 
+                                                   split='test', 
+                                                   target_type='attr', 
+                                                   download=False, 
+                                                   transform=transform,
+                                                   target_transform=get_smile)
+
+celeba_train_dataset = Subset(celeba_train_dataset, torch.arange(16000)) 
+celeba_valid_dataset = Subset(celeba_valid_dataset, torch.arange(1000)) 
+ 
+print('Train set:', len(celeba_train_dataset))
+print('Validation set:', len(celeba_valid_dataset))
+
+
+
+
+batch_size = 32
+
+torch.manual_seed(1)
+train_dl = DataLoader(celeba_train_dataset, batch_size, shuffle=True)
+valid_dl = DataLoader(celeba_valid_dataset, batch_size, shuffle=False)
+test_dl = DataLoader(celeba_test_dataset, batch_size, shuffle=False)
+
+
+# ### Training a CNN gender classifier
+# 
+# * **Global Average Pooling**
+
+
+
+
+
+
+
+
+model = nn.Sequential()
+
+model.add_module('conv1', nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1))
+model.add_module('relu1', nn.ReLU())        
+model.add_module('pool1', nn.MaxPool2d(kernel_size=2))  
+model.add_module('dropout1', nn.Dropout(p=0.5)) 
+
+model.add_module('conv2', nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1))
+model.add_module('relu2', nn.ReLU())        
+model.add_module('pool2', nn.MaxPool2d(kernel_size=2))   
+model.add_module('dropout2', nn.Dropout(p=0.5)) 
+
+model.add_module('conv3', nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1))
+model.add_module('relu3', nn.ReLU())        
+model.add_module('pool3', nn.MaxPool2d(kernel_size=2))   
+
+model.add_module('conv4', nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1))
+model.add_module('relu4', nn.ReLU())  
+
+
+
+
+x = torch.ones((4, 3, 64, 64))
+model(x).shape
+
+
+
+
+model.add_module('pool4', nn.AvgPool2d(kernel_size=8)) 
+model.add_module('flatten', nn.Flatten()) 
+
+x = torch.ones((4, 3, 64, 64))
+model(x).shape
+
+
+
+
+model.add_module('fc', nn.Linear(256, 1)) 
+model.add_module('sigmoid', nn.Sigmoid()) 
+
+
+
+
+x = torch.ones((4, 3, 64, 64))
+model(x).shape
+
+
+
+
+model
+
+
+
+
+device = torch.device("cuda:0")
+# device = torch.device("cpu")
+model = model.to(device) 
+
+
+
+
+loss_fn = nn.BCELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+def train(model, num_epochs, train_dl, valid_dl):
+    loss_hist_train = [0] * num_epochs
+    accuracy_hist_train = [0] * num_epochs
+    loss_hist_valid = [0] * num_epochs
+    accuracy_hist_valid = [0] * num_epochs
+    for epoch in range(num_epochs):
+        model.train()
+        for x_batch, y_batch in train_dl:
+            x_batch = x_batch.to(device) 
+            y_batch = y_batch.to(device) 
+            pred = model(x_batch)[:, 0]
+            loss = loss_fn(pred, y_batch.float())
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            loss_hist_train[epoch] += loss.item()*y_batch.size(0)
+            is_correct = ((pred>=0.5).float() == y_batch).float()
+            accuracy_hist_train[epoch] += is_correct.sum().cpu()
+
+        loss_hist_train[epoch] /= len(train_dl.dataset)
+        accuracy_hist_train[epoch] /= len(train_dl.dataset)
+        
+        model.eval()
+        with torch.no_grad():
+            for x_batch, y_batch in valid_dl:
+                x_batch = x_batch.to(device) 
+                y_batch = y_batch.to(device) 
+                pred = model(x_batch)[:, 0]
+                loss = loss_fn(pred, y_batch.float())
+                loss_hist_valid[epoch] += loss.item()*y_batch.size(0) 
+                is_correct = ((pred>=0.5).float() == y_batch).float()
+                accuracy_hist_valid[epoch] += is_correct.sum().cpu()
+
+        loss_hist_valid[epoch] /= len(valid_dl.dataset)
+        accuracy_hist_valid[epoch] /= len(valid_dl.dataset)
+        
+        print(f'Epoch {epoch+1} accuracy: {accuracy_hist_train[epoch]:.4f} val_accuracy: {accuracy_hist_valid[epoch]:.4f}')
+    return loss_hist_train, loss_hist_valid, accuracy_hist_train, accuracy_hist_valid
+
+torch.manual_seed(1)
+num_epochs = 30
+hist = train(model, num_epochs, train_dl, valid_dl)
+
+
+
+
+x_arr = np.arange(len(hist[0])) + 1
+
+fig = plt.figure(figsize=(12, 4))
+ax = fig.add_subplot(1, 2, 1)
+ax.plot(x_arr, hist[0], '-o', label='Train loss')
+ax.plot(x_arr, hist[1], '--<', label='Validation loss')
+ax.legend(fontsize=15)
+ax.set_xlabel('Epoch', size=15)
+ax.set_ylabel('Loss', size=15)
+
+ax = fig.add_subplot(1, 2, 2)
+ax.plot(x_arr, hist[2], '-o', label='Train acc.')
+ax.plot(x_arr, hist[3], '--<', label='Validation acc.')
+ax.legend(fontsize=15)
+ax.set_xlabel('Epoch', size=15)
+ax.set_ylabel('Accuracy', size=15)
+
+#plt.savefig('figures/14_17.png', dpi=300)
+plt.show()
+
+
+
+
+accuracy_test = 0
+
+model.eval()
+with torch.no_grad():
+    for x_batch, y_batch in test_dl:
+        x_batch = x_batch.to(device) 
+        y_batch = y_batch.to(device) 
+        pred = model(x_batch)[:, 0]
+        is_correct = ((pred>=0.5).float() == y_batch).float()
+        accuracy_test += is_correct.sum().cpu()
+ 
+accuracy_test /= len(test_dl.dataset)
+        
+print(f'Test accuracy: {accuracy_test:.4f}') 
+
+
+
+
+pred = model(x_batch)[:, 0] * 100
+
+fig = plt.figure(figsize=(15, 7))
+for j in range(10, 20):
+    ax = fig.add_subplot(2, 5, j-10+1)
+    ax.set_xticks([]); ax.set_yticks([])
+    ax.imshow(x_batch[j].cpu().permute(1, 2, 0))
+    if y_batch[j] == 1:
+        label = 'Smile'
+    else:
+        label = 'Not Smile'
+    ax.text(
+        0.5, -0.15, 
+        f'GT: {label:s}\nPr(Smile)={pred[j]:.0f}%', 
+        size=16, 
+        horizontalalignment='center',
+        verticalalignment='center', 
+        transform=ax.transAxes)
+    
+#plt.savefig('figures/figures-14_18.png', dpi=300)
+plt.show()
+
+
+
+
+path = 'models/celeba-cnn.ph'
+torch.save(model, path)
+
+
+# ...
+# 
+# 
+# ## Summary
+# 
+# ...
+# 
+# 
+
+# ----
+# 
+# Readers may ignore the next cell.
+
+
+
+
+
+
+
+
+
