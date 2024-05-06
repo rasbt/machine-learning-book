011c99c06af23a71b3e0b8db7de9784f2b2905de
Jeffrey Perkinson <jperkinson@jeffs-geekom>
2024-05-06 08:16:03 -0400

    add iris feature plot.

--- ch02/perceptron-walkthrough/iris.py
+++ ch02/perceptron-walkthrough/iris.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 import perceptron
 
@@ -8,4 +10,25 @@ s = '/home/jperkinson/dev/machine-learning-book-jperk224/ch02/iris.data'
 
 df = pd.read_csv(s, header=None, encoding='utf-8')
 
-print(df.tail())
+# print(df.tail())
+
+# select setosa and versicolor class labels
+# take the 4th column (the class label) of the first 100 rows
+# these are the class labels of interest for this exercise
+y = df.iloc[0:100, 4]
+y = np.where(y == 'Iris-setosa', 0, 1)
+
+# extract sepal length and petal length into our feature matrix
+X = df.iloc[0:100, [0,2]].values
+
+# plot the data
+# sepal length (x) and petal length (y) for Setosa (1st 50 rows of the data set)
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='Setosa')
+
+# sepal length (x) and petal length (y) for Versicolor (Last 50 rows of the data set)
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='s', label='Versicolor')
+
+plt.xlabel('Sepal length [cm]')
+plt.ylabel('Petal length [cm]')
+plt.legend(loc='upper left')
+plt.show()
